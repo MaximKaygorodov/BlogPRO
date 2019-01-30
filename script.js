f2cb6bee02c1faea8570bd354a572604949d1c5b
MaximKaygorodov <47054248+MaximKaygorodov@users.noreply.github.com>
2019-01-30 15:44:02 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Выезд меню
-$('.menu-btn').on('click', function(e) {
-    e.preventDefault();
-    $('.menu').toggleClass('menu_active');
-    $('.content').toggleClass('content_active');
+$(document).ready(function(){
+    // Initialize Tooltip
+    $('[data-toggle="tooltip"]').tooltip(); 
   })
